01621af50fafde0c158d1fee3344c00489aee1a8
TechSmiths23 <144578774+TechSmiths23@users.noreply.github.com>
2023-10-01 20:07:40 -0500

    Update TestCode.py

--- TestCode.py
+++ TestCode.py
@@ -1,6 +1,38 @@
 import RPi.GPIO as GPIO
+import netifaces as ni
+import socket
+pip install gpiozero
+
 #Need to Figure out what module to import based on motor drivers
 #GPIO Pin set up
 
+# Create a TCP/IP server socket
+server_ip = '169.254.189.163'  # Listen on all available interfaces
+port = 6000  # Match the port number used in MATLAB
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((server_ip, port))
+server_socket.listen(1)
+
+def check_ethernet_status(interface="eth0"):
+    try:
+        ip = ni.ifaddresses(interface)[ni.AF_INET][0]['addr']
+        return True
+    except ValueError:
+        return False
+
+if check_ethernet_status():
+    print("Ethernet cable is connected.")
+    print(f"Listening on {server_ip}:{port}...")
+
+    conn, addr = server_socket.accept()
+    print(f"Connected by {addr}")
+
+    while True:
+      command = conn.recv(1024).decode()
+    if not command:
+        break
+else:
+    print("Ethernet cable is not connected.")
 
 
