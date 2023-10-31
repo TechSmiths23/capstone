3002696b7ff999e7a7856780ecdc7dda3b182fd7
TechSmiths23 <144578774+TechSmiths23@users.noreply.github.com>
2023-10-30 22:37:35 -0500

    Create motor_functions_re_10_30.py

---
+++ motor_functions_re_10_30.py
@@ -0,0 +1,184 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+# Motor 1
+ENA = 18
+IN1 = 22
+IN2 = 23
+
+# Motor 2
+ENB = 19
+IN3 = 20
+IN4 = 16
+
+# Motor 5
+ENC = 13
+IN5 = 21
+IN6 = 27
+
+# Motor 4
+END = 24
+IN7 = 6
+IN8 = 12
+
+# Motor 6
+ENE = 7
+IN9 = 17
+IN10 = 5
+
+# Motor 3
+ENF = 8
+IN11 = 4
+IN12 = 25
+
+temp1 = 1
+GPIO.setmode(GPIO.BCM)
+
+# GPIO pin setup for motor 1 PWM
+GPIO.setup(IN1, GPIO.OUT)
+GPIO.setup(IN2, GPIO.OUT)
+GPIO.setup(ENA, GPIO.OUT)
+
+# GPIO pin setup for motor 2 PWM
+GPIO.setup(IN3, GPIO.OUT)
+GPIO.setup(IN4, GPIO.OUT)
+GPIO.setup(ENB, GPIO.OUT)
+
+# GPIO pin setup for motor 3 PWM
+GPIO.setup(IN5, GPIO.OUT)
+GPIO.setup(IN6, GPIO.OUT)
+GPIO.setup(ENC, GPIO.OUT)
+
+# GPIO pin setup for motor 4 PWM
+GPIO.setup(IN7, GPIO.OUT)
+GPIO.setup(IN8, GPIO.OUT)
+GPIO.setup(END, GPIO.OUT)
+
+# GPIO pin setup for motor 5
+GPIO.setup(IN9, GPIO.OUT)
+GPIO.setup(IN10, GPIO.OUT)
+GPIO.setup(ENE, GPIO.OUT)
+
+# GPIO pin setup for motor 6
+GPIO.setup(IN11, GPIO.OUT)
+GPIO.setup(IN12, GPIO.OUT)
+GPIO.setup(ENF, GPIO.OUT)
+
+# Output setup
+GPIO.output(IN1, GPIO.LOW)
+GPIO.output(IN2, GPIO.LOW)
+
+GPIO.output(IN3, GPIO.LOW)
+GPIO.output(IN4, GPIO.LOW)
+
+GPIO.output(IN5, GPIO.LOW)
+GPIO.output(IN6, GPIO.LOW)
+
+GPIO.output(IN7, GPIO.LOW)
+GPIO.output(IN8, GPIO.LOW)
+
+GPIO.output(IN9, GPIO.LOW)
+GPIO.output(IN10, GPIO.LOW)
+
+GPIO.output(IN11, GPIO.LOW)
+GPIO.output(IN12, GPIO.LOW)
+
+p1 = GPIO.PWM(ENA, 1000)
+p2 = GPIO.PWM(ENB, 1000)
+p3 = GPIO.PWM(ENC, 1000)
+p4 = GPIO.PWM(END, 1000)
+p5 = GPIO.PWM(ENE, 1000)
+p6 = GPIO.PWM(ENF, 1000)
+
+p1.start(25)
+p2.start(25)
+p3.start(25)
+p4.start(25)
+p5.start(25)
+p6.start(25)
+
+print("\n")
+print("The default speed & direction of the motor is LOW & Forward.....")
+print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
+print("\n")
+
+while True:
+    x = input()
+
+    if x == 'r':
+        print("run")
+        if temp1 == 1:
+            GPIO.output(IN1, GPIO.HIGH)  # motor 1
+            GPIO.output(IN2, GPIO.LOW)  # motor 1
+            GPIO.output(IN7, GPIO.HIGH)  # motor 4
+            GPIO.output(IN8, GPIO.LOW)  # motor 4
+            print("forward")
+            x = 'z'
+
+    elif x == 's':
+        print("stop")
+        GPIO.output(IN1, GPIO.LOW)
+        GPIO.output(IN2, GPIO.LOW)
+        x = 'z'
+
+    elif x == 'f':
+        print("forward")
+        GPIO.output(IN3, GPIO.HIGH)  # motor 2
+        GPIO.output(IN4, GPIO.LOW)  # motor 2
+        GPIO.output(IN5, GPIO.HIGH)  # motor 5
+        GPIO.output(IN6, GPIO.LOW)  # motor 5
+        temp1 = 1
+        x = 'z'
+
+    elif x == 'b':
+        print("backward")
+        # Your code here
+        x = 'z'
+
+    elif x == 'l':
+        print("turn left")
+        # Your code here
+        x = 'z'
+
+    elif x == 'r':
+        print("turn right")
+        # Your code here
+        x = 'z'
+
+    elif x == '1':
+        print("low-1")
+        p1.ChangeDutyCycle(25)
+        p2.ChangeDutyCycle(25)
+        p3.ChangeDutyCycle(25)
+        p4.ChangeDutyCycle(25)
+        p5.ChangeDutyCycle(25)
+        p6.ChangeDutyCycle(25)
+        x = 'z'
+
+    elif x == '2':
+        print("medium-2")
+        p1.ChangeDutyCycle(50)
+        p2.ChangeDutyCycle(50)
+        p3.ChangeDutyCycle(50)
+        p4.ChangeDutyCycle(50)
+        p5.ChangeDutyCycle(50)
+        p6.ChangeDutyCycle(50)
+        x = 'z'
+
+    elif x == '3':
+        print("high-3")
+        p1.ChangeDutyCycle(75)
+        p2.ChangeDutyCycle(75)
+        p3.ChangeDutyCycle(75)
+        p4.ChangeDutyCycle(75)
+        p5.ChangeDutyCycle(75)
+        p6.ChangeDutyCycle(75)
+        x = 'z'
+
+    elif x == 'e':
+        GPIO.cleanup()
+        break
+
+    else:
+        print("<<<  wrong data  >>>")
+        print("please Enter the defined data to continue.....")
