989d6b73899ea50d4667e814df15bf256e34762d
TechSmiths23 <144578774+TechSmiths23@users.noreply.github.com>
2023-11-07 17:14:13 -0600

    Update camera_test.py

--- camera_test.py
+++ camera_test.py
@@ -2,26 +2,19 @@ import cv2
 import numpy as np
 import socket
 
-cap = cv2.VideoCapture(0)  # Access the USB camera (adjust the index if necessary)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('YOUR_LAPTOP_IP', YOUR_PORT))  # Use the same port as on the Raspberry Pi
+s.listen(5)
 
 while True:
-    ret, frame = cap.read()
-    # Convert the frame to bytes
-    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-    _, frame = cv2.imencode('.jpg', frame, encode_param)
-    data = np.array(frame)
-    stringData = data.tostring()
-
-    # Create a socket connection
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('YOUR_LAPTOP_IP', YOUR_PORT))  # Replace with your laptop's IP and a chosen port
-
-    # Send the frame data
-    s.sendall((str(len(stringData))).encode().ljust(16) + stringData)
-    s.close()
+    conn, addr = s.accept()
+    length = int(conn.recv(16))
+    stringData = conn.recv(length)
+    data = np.frombuffer(stringData, dtype='uint8')
+    decimg = cv2.imdecode(data, 1)
+    cv2.imshow('Video', decimg)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-cap.release()
 cv2.destroyAllWindows()
